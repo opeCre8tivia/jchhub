b3a8eb129a246a45389d6bf825d5f9deeffb7dd9
opeCre8tivia <opegude.n3t@gmail.com>
2024-07-07 08:46:10 +0300

    setup:configured Ant Design | Redux toolkit | react-router-dom

---
+++ frontend/src/constants/colors.ts
@@ -0,0 +1,11 @@
+export const colors = {
+  primary: "#AB2125",
+  secondary: "#39BCA5",
+  dark: "#04223E",
+  black: "#000000",
+  white: "#ffffff",
+  gray50: "#FAFAFA",
+  gray100: "#C4C4C4",
+  gray200: "#818C88",
+  danger: "#FF0000"
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+//ant design
+import { ConfigProvider } from "antd"
+import { colors } from "./constants/colors.ts"
+// redux
+import { Provider } from "react-redux"
+import { store } from "./redux/store.ts"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+// react router dom
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+//routes TODO move to a nother file
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <ConfigProvider
+      theme={{
+        token: {
+          // Seed Token
+          colorPrimary: colors.primary,
+          borderRadius: 6,
+
+          // Alias Token
+          colorBgContainer: colors.white
+        }
+      }}
+    >
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+    </ConfigProvider>
+  </Provider>
 )
